b5b6b06bb9fa2c112a2019e68d760f0b801f9771
Jared Tanner <Deraj21@gmail.com>
2020-02-24 22:44:52 -0700

    initial file structure

---
+++ src/components/Encounter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Encounter extends Component {
+  render(){
+
+    return (
+      <div className="Encounter">
+        <h2>Encounter Page</h2>
+      </div>
+    );
+  }
+}
+
+export default Encounter
